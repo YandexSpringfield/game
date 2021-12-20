a175d7373a0fa4456a92ed0d7fe90c71a2aa4072
tigfamon <tigfamon@gmail.com>
2021-12-20 21:11:46 +0300

    Initialize game page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { routes } from '@appConstants';
-import { Login, Registration } from '@pages';
+import { Login, Registration, Game } from '@pages';
 
 export const App = () => {
   return (
     <Router>
-      <nav>
-        <Link to={routes.login}>Login page</Link>
-        <Link to={routes.registration}>Registration page</Link>
-      </nav>
       <Routes>
         <Route path={routes.login} element={<Login />} />
         <Route path={routes.registration} element={<Registration />} />
+        <Route path={routes.game} element={<Game />} />
       </Routes>
     </Router>
   );
