7af7ee8bf42608ac2367cda2efc25d48508168ab
verobragina <verobragina@ya.ru>
2021-12-24 19:02:49 +0300

    Add Provider to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { routes } from '@appConstants';
 import { Login, Registration } from '@pages';
+import { store } from '@store';
 
 export const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path={routes.login} element={<Login />} />
-        <Route path={routes.registration} element={<Registration />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path={routes.login} element={<Login />} />
+          <Route path={routes.registration} element={<Registration />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 };
