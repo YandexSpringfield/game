338239c11f1bae6e6d6ad2813fed11f7cd81ea6d
tigfamon <tigfamon@gmail.com>
2021-12-25 00:27:43 +0300

    Add styles for Content.tsx component

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -2,10 +2,12 @@ import React, { FC } from 'react';
 import cn from 'classnames';
 import { TProps } from '.';
 
+import styles from './styles.module.scss';
+
 export const Content: FC<TProps> = ({ title, children, className }) => {
   return (
-    <main>
-      <h1>{title}</h1>
+    <main className={styles.container}>
+      <h1 className={styles.title}>{title}</h1>
       <section className={cn(className)}>{children}</section>
     </main>
   );
