3c4d5fbd900926e8dbf445d74ea47fb2c6282694
tigfamon <tigfamon@gmail.com>
2021-12-28 20:25:29 +0300

    Fix Avatar.tsx

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -4,10 +4,8 @@ import { TProps } from '.';
 
 import styles from './styles.module.scss';
 
-export const Avatar: FC<TProps> = ({ className, src, alt }) => {
-  return (
-    <div className={cn(styles.avatar, className)}>
-      <img className={styles.img} src={src} alt={alt || 'Avatar'} />
-    </div>
-  );
-};
+export const Avatar: FC<TProps> = ({ className, src, alt = 'Avatar' }) => (
+  <div className={cn(styles.avatar, className)}>
+    <img className={styles.img} src={src} alt={alt} />
+  </div>
+);
