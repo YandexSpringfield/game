d2ddaee29a86f84942ac5ea3d82608ed9988cd8e
verobragina <verobragina@ya.ru>
2022-01-02 15:27:15 +0300

    Change Leaderboard layout

--- src/components/TableRow/TableRow.tsx
+++ src/components/TableRow/TableRow.tsx
@@ -6,12 +6,12 @@ import defaultAvatar from '../../assets/images/default-logo.png';
 import styles from './TableRow.module.scss';
 
 export const TableRow: FC<TProps> = ({ rank, avatar, username, score }) => (
-  <tr className={styles.row}>
-    <td>{rank}</td>
-    <td className={styles.player}>
+  <div className={styles.row}>
+    <div>{rank}</div>
+    <div className={styles.player}>
       <Avatar src={avatar || defaultAvatar} />
       {username}
-    </td>
-    <td>{score}</td>
-  </tr>
+    </div>
+    <div>{score}</div>
+  </div>
 );
--- src/containers/Leaderboard/Leaderboard.tsx
+++ src/containers/Leaderboard/Leaderboard.tsx
@@ -6,19 +6,15 @@ import styles from './styles.module.scss';
 
 export const Leaderboard = () => (
   <Content title="Leaderboard">
-    <table className={styles.table}>
-      <thead>
-        <tr className={styles.header}>
-          <th>№</th>
-          <th>Игрок</th>
-          <th>Счет</th>
-        </tr>
-      </thead>
-      <tbody>
+    <div className={styles.table}>
+      <div className={styles.header}>
+        <div>№</div>
+        <div>Игрок</div>
+        <div>Счет</div>
+      </div>
         {MOCK_DATA.map(({ rank, ...item }) => (
           <TableRow key={rank} rank={rank} {...item} />
         ))}
-      </tbody>
-    </table>
+    </div>
   </Content>
 );
