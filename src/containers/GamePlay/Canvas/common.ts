27cdc2ff9f8d506fc606669b42d34a49445f1c35
tigfamon <tigfamon@gmail.com>
2022-01-12 23:40:58 +0700

    Fix loadImage fn code style

--- src/containers/GamePlay/Canvas/common.ts
+++ src/containers/GamePlay/Canvas/common.ts
@@ -1,9 +1,8 @@
-export function loadImage(url: string): Promise<HTMLImageElement> {
-  return new Promise((resolve) => {
+export const loadImage = (url: string): Promise<HTMLImageElement> =>
+  new Promise((resolve) => {
     const image = new Image();
     image.addEventListener('load', () => {
       resolve(image);
     });
     image.src = url;
   });
-}
