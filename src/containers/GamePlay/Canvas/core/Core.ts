70d8b31100398810a4fca8afa259cf7b0ea365bc
tigfamon <tigfamon@gmail.com>
2022-01-12 23:38:49 +0700

    Fix drawSky and drawGround methods

--- src/containers/GamePlay/Canvas/core/Core.ts
+++ src/containers/GamePlay/Canvas/core/Core.ts
@@ -1,9 +1,19 @@
-/* eslint-disable no-plusplus */
 import { loadImage, SpriteResolver } from '@containers/GamePlay/Canvas';
 import spriteImage from '@/assets/images/sprite.png';
 
-const skyRange = [[0, 40, 0, 18]];
-const groundRange = [[0, 40, 18, 20]];
+const groundRange = {
+  x1: 0,
+  x2: 40,
+  y1: 18,
+  y2: 20,
+};
+
+const skyRange = {
+  x1: 0,
+  x2: 40,
+  y1: 0,
+  y2: 18,
+};
 
 export class Core {
   public static _instance: Core;
@@ -12,53 +22,44 @@ export class Core {
 
   public context: CanvasRenderingContext2D;
 
-  public spriteImage: HTMLImageElement | null;
+  public sprite: SpriteResolver;
 
   constructor(canvas: HTMLCanvasElement) {
-    Core._instance = this;
     this.canvas = canvas;
     this.context = this.canvas.getContext('2d') as CanvasRenderingContext2D;
-    this.spriteImage = null;
   }
 
   init() {
-    this.drawBackgrounds();
-    this.drawMario();
-  }
-
-  private drawBackgrounds() {
     loadImage(spriteImage).then((image) => {
-      this.spriteImage = image;
-
-      const sprites = new SpriteResolver(this.spriteImage);
-      sprites.defineTile('ground', 0, 0);
-      sprites.defineTile('sky', 1, 0);
-
-      groundRange.forEach(([x1, x2, y1, y2]) => {
-        for (let x = x1; x < x2; ++x) {
-          for (let y = y1; y < y2; ++y) {
-            sprites.drawTile('ground', this.context, x, y);
-          }
-        }
-      });
-
-      skyRange.forEach(([x1, x2, y1, y2]) => {
-        for (let x = x1; x < x2; ++x) {
-          for (let y = y1; y < y2; ++y) {
-            sprites.drawTile('sky', this.context, x, y);
-          }
-        }
-      });
-
+      this.sprite = new SpriteResolver(image);
+      this.drawGround();
+      this.drawSky();
       this.drawMario();
     });
   }
 
-  private drawMario() {
-    if (this.spriteImage) {
-      const sprites = new SpriteResolver(this.spriteImage);
-      sprites.define('mario', 64, 0, 32, 32);
-      sprites.draw('mario', this.context, 32, 544);
+  private drawGround() {
+    this.sprite.defineTile('ground', 0, 0);
+
+    for (let i = groundRange.x1; i <= groundRange.x2; i += 1) {
+      for (let j = groundRange.y1; j < groundRange.y2; j += 1) {
+        this.sprite.drawTile('ground', this.context, i, j);
+      }
     }
+  }
+
+  private drawSky() {
+    this.sprite.defineTile('sky', 1, 0);
+
+    for (let i = skyRange.x1; i <= skyRange.x2; i += 1) {
+      for (let j = skyRange.y1; j < skyRange.y2; j += 1) {
+        this.sprite.drawTile('sky', this.context, i, j);
+      }
+    }
+  }
+
+  private drawMario() {
+    this.sprite.define('mario', 64, 0, 32, 32);
+    this.sprite.draw('mario', this.context, 32, 544);
   }
 }
