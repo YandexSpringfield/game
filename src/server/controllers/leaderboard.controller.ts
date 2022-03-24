971e46656cf69b08c1a9c65e6a51b19b6c695e24
tigfamon <tigfamon@gmail.com>
2022-03-24 11:41:30 +0300

    Fix leaderboard url

--- src/server/controllers/leaderboard.controller.ts
+++ src/server/controllers/leaderboard.controller.ts
@@ -8,9 +8,13 @@ export const leaderboardRoute = Router();
 async function get(req: PrivateRequest, res: Response) {
   try {
     const { cookies, body } = req;
-    const response = await apiInstance.post(`/api/v2/leaderboard/all`, body, {
-      headers: setAuthCookies(cookies.authCookie, cookies.uuid),
-    });
+    const response = await apiInstance.post(
+      `/api/v2/leaderboard/springfieldMario`,
+      body,
+      {
+        headers: setAuthCookies(cookies.authCookie, cookies.uuid),
+      },
+    );
     res.status(200).send({
       success: true,
       data: response.data,
